59d797762966f3b581705008b07d843c8618f054
ciceiOSAndroid <pablo.mateo.pruebas2@gmail.com>
2019-02-07 09:21:48 +0100

    Inicio Proyecto

---
+++ app/src/main/java/com/m600/alumnos/cice/vinum/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.m600.alumnos.cice.vinum
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    //App Vinum
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
