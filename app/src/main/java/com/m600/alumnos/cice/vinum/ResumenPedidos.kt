e22c606804c05765052b8af20c72b15e1b989ea7
julian <julian.ramos.calvo@gmail.com>
2019-02-09 19:23:18 +0100

    Creación del Empty Activity

---
+++ app/src/main/java/com/m600/alumnos/cice/vinum/ResumenPedidos.kt
@@ -0,0 +1,12 @@
+package com.m600.alumnos.cice.vinum
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ResumenPedidos : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_resumen_pedidos)
+    }
+}
