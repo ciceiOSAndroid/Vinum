20e371d57e44199a8f8878a03dbe55f68519d083
HildaS <hildasalas@gmail.com>
2019-02-13 18:30:22 +0100

    Creación del CameraVinumActivity

---
+++ app/src/main/java/com/m600/alumnos/cice/vinum/CameraVinumActivity.kt
@@ -0,0 +1,12 @@
+package com.m600.alumnos.cice.vinum
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CameraVinumActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_camera_vinum)
+    }
+}
